d523f53cfd7429f06f11f958df397cc2496dbb28
rencan97 <work.rencan@gmail.com>
2023-03-09 14:33:22 -0300

    setup

--- medical_data_visualizer/medical_data_visualizer.py
+++ medical_data_visualizer/medical_data_visualizer.py
@@ -1,50 +1,62 @@
-import unittest
-import medical_data_visualizer
-import matplotlib as mpl
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data
+df = pd.read_csv("medical_examination.csv")
+df.head()
 
 
-# the test case
-class CatPlotTestCase(unittest.TestCase):
-    def setUp(self):
-        self.fig = medical_data_visualizer.draw_cat_plot()
-        self.ax = self.fig.axes[0]
+# Add 'overweight' column
+def map_overweight(value):
+    if value > 25: return 1
+    else: return 0
+df['overweight'] = (df['weight']/(df['height']/100)**2).map(map_overweight)
+
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+def applymap_normalize(value):
+    if value > 1: return 1
+    else: return 0
+df[['cholesterol', 'gluc']] = df[['cholesterol', 'gluc']].applymap(applymap_normalize)
+# Draw Categorical Plot
+def draw_cat_plot():
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars="cardio", value_vars= ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat = df_cat.groupby("cardio").value_counts().to_frame("total").reset_index()
     
-    def test_line_plot_labels(self):
-        actual = self.ax.get_xlabel()
-        expected = "variable"
-        self.assertEqual(actual, expected, "Expected line plot xlabel to be 'variable'")
-        actual = self.ax.get_ylabel()
-        expected = "total"
-        self.assertEqual(actual, expected, "Expected line plot ylabel to be 'total'")
-        actual = []
-        for label in self.ax.get_xaxis().get_majorticklabels():
-            actual.append(label.get_text())
-        expected = ['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke']
-        self.assertEqual(actual, expected, "Expected bar plot secondary x labels to be 'active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'")
-
-    def test_bar_plot_number_of_bars(self):
-        actual = len([rect for rect in self.ax.get_children() if isinstance(rect, mpl.patches.Rectangle)])
-        expected = 13
-        self.assertEqual(actual, expected, "Expected a different number of bars chart.")
+    # Draw the catplot with 'sns.catplot()'
+    # Get the figure for the output
+    fig = sns.catplot(df_cat, x = "variable", order = df_cat['variable'].sort_values().unique(), y = "total", col = "cardio", hue = "value", kind="bar").fig
 
 
-class HeatMapTestCase(unittest.TestCase):
-    def setUp(self):
-        self.fig = medical_data_visualizer.draw_heat_map()
-        self.ax = self.fig.axes[0]
+    # Do not modify the next two lines
+    fig.savefig('catplot.png')
+    return fig
 
-    def test_heat_map_labels(self):
-        actual = []
-        for label in self.ax.get_xticklabels():
-          actual.append(label.get_text())
-        expected = ['id', 'age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight']
-        self.assertEqual(actual, expected, "Expected heat map labels to be 'id', 'age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight'.")
+# Draw Heat Map
+def draw_heat_map():
+    # Clean the data
+    df_heat = df.loc[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] <= df['weight'].quantile(0.975))]
+
+    print(df_heat)
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+
+
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots()
+
+    # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", ax = ax)
     
-    def test_heat_map_values(self):
-        actual = [text.get_text() for text in self.ax.get_default_bbox_extra_artists() if isinstance(text, mpl.text.Text)]
-        print(actual)
-        expected = ['0.0', '0.0', '-0.0', '0.0', '-0.1', '0.5', '0.0', '0.1', '0.1', '0.3', '0.0', '0.0', '0.0', '0.0', '0.0', '0.0', '0.2', '0.1', '0.0', '0.2', '0.1', '0.0', '0.1', '-0.0', '-0.1', '0.1', '0.0', '0.2', '0.0', '0.1', '-0.0', '-0.0', '0.1', '0.0', '0.1', '0.4', '-0.0', '-0.0', '0.3', '0.2', '0.1', '-0.0', '0.0', '0.0', '-0.0', '-0.0', '-0.0', '0.2', '0.1', '0.1', '0.0', '0.0', '0.0', '0.0', '0.3', '0.0', '-0.0', '0.0', '-0.0', '-0.0', '-0.0', '0.0', '0.0', '-0.0', '0.0', '0.0', '0.0', '0.2', '0.0', '-0.0', '0.2', '0.1', '0.3', '0.2', '0.1', '-0.0', '-0.0', '-0.0', '-0.0', '0.1', '-0.1', '-0.1', '0.7', '0.0', '0.2', '0.1', '0.1', '-0.0', '0.0', '-0.0', '0.1']
-        self.assertEqual(actual, expected, "Expected different values in heat map.")
+    # Do not modify the next two lines
+    fig.savefig('heatmap.png')
+    return fig
 
-if __name__ == "__main__":
-    unittest.main()
+draw_heat_map()
